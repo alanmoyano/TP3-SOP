259509afa7d1dcd0e15ec9b6c441f3073141b275
Alan (linux) <alanmoyano203@gmail.com>
2023-10-11 16:34:59 -0300

    Cambios en el formato y agregados comentarios

--- reporte.sh
+++ reporte.sh
@@ -8,33 +8,31 @@ TEMPORAL="./temporal.tmp"
 # Creamos el archivo temporal con los datos del archivo especial "/proc/meminfo" sin los espacios adicionales
 cat /proc/meminfo | tr -s " " >$TEMPORAL
 
-# Creamos todas las variables que refieren a cada campo necesario para el reporte
-# Las formulas usadas fueron sacadas de `man free`
+# Creamos la variable que contendrá la hora de ejecución del script
 hora=$(date | cut -d " " -f4 | cut -d ":" -f1,2)
 
+# Creamos todas las variables que refieren a cada campo necesario para el reporte
+# Las formulas usadas fueron sacadas de `man free`
 total=$(cat $TEMPORAL | grep "MemTotal" | cut -d ' ' -f2)
-
 libre=$(cat $TEMPORAL | grep "MemFree" | cut -d ' ' -f2)
-
 buffers=$(cat $TEMPORAL | grep "Buffers" | cut -d ' ' -f2)
-
 cache=$(expr $(cat $TEMPORAL | grep -w "Cached" | cut -d ' ' -f2) + $(cat $TEMPORAL | grep "SReclaimable" | cut -d ' ' -f2))
-
 disponible=$(cat $TEMPORAL | grep "Available" | cut -d ' ' -f2)
 
+# Calculamos finalmente la memoria usada
 usada=$(expr $total - $libre - $buffers - $cache)
 
 # Se genera y añade el reporte al archivo correspondiente
 echo "$hora|$total|$usada|$libre|$buffers|$cache|$disponible|" >>~/ReporteDiario
 
-# Se verifica que el reporte no haya excedido las líneas máximas por día
+# Se verifica la existencia del reporte (por las dudas)
 if [ -e ~/ReporteDiario ]; then
 
+	# Se verifica que el reporte no haya excedido las líneas máximas por día
 	# Si las excede, se actualiza el ReporteDiarioAnterior y se reinicia el ReporteDiario
-	if [ $(cat ~/ReporteDiario | wc -l) -eq 1440 ]; then
+	if [ $(cat ~/ReporteDiario | wc -l) -ge 1440 ]; then
 		mv ~/ReporteDiario ~/ReporteDiarioAnterior
 	fi
-
 fi
 
 # Se elimina el archivo temporal auxiliar
